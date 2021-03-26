1b340442c9bb354307806092dd8816a439231428
ONKAR KOLVANKAR <ONKARKOLVANKAR@gmail.com>
2021-03-26 17:24:52 +0530

    Canvas and directions Added
    - Added the Canvas and placeholders for the Lap time and Positions.
    - added the DirectionalTrigger script which is added to the triggers which shows the directions displayed on the Image UI component

---
+++ Death Race/Assets/Scripts/DirectionTriggers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DirectionTriggers : MonoBehaviour
+{
+    [SerializeField] Sprite directionSignTexture;
+    // [SerializeField] RawImage directionsRawImage;
+    [SerializeField] Image directionsImage;
+
+
+    private void Start()
+    {
+        directionsImage.GetComponent<Image>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            directionsImage.enabled = true;
+            directionsImage.sprite = directionSignTexture;
+
+            Invoke("setDirectionsNone", 2f);
+        }
+    }
+
+    private void setDirectionsNone() {
+        directionsImage.sprite = null;
+        directionsImage.enabled = false;
+    }
+}
