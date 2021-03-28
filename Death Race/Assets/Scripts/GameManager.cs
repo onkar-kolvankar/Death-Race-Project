d8bd86314b547f0468646da562a68f7fe2fa4ed6
ONKAR KOLVANKAR <ONKARKOLVANKAR@gmail.com>
2021-03-28 11:26:01 +0530

    Tried to code for Menu Panels
    - Added the GameManager script
    - No functionalities added just sets values of the variables.
    - Adjusted the UI in the Sample Scene where added 2 panels. one for singleplayer TimeRush category, another for 2 player race .

--- Death Race/Assets/Scripts/GameManager.cs
+++ Death Race/Assets/Scripts/GameManager.cs
@@ -5,7 +5,20 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // This field will be initialized when we select Single player/ multiplayer.
-    [SerializeField] int o_totalPlayerCount = 1;
+    public int o_totalPlayerCount = 1;
+
+    public int o_lapCountTotal = 3;
+
+    // Initialize the prefabs of the all cars.
+    public GameObject o_car1Prefab;
+    public GameObject o_car2Prefab;
+    public GameObject o_car3Prefab;
+
+    public string o_gameMode;
+    public string o_trackSelected;
+
+    //public CarManager[] o_Cars;
+
     
 
     // Start is called before the first frame update
---
+++ Death Race/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    GameManager o_gameManager;
+
+    void Start() {
+        o_gameManager = FindObjectOfType<GameManager>();
+    }
+
+    // --------------- Main Menu -----------------
+    public void o_SetModeSinglePlayer()
+    {
+        o_gameManager.o_gameMode = "Singleplayer";
+        o_gameManager.o_totalPlayerCount = 1;
+    }
+
+    public void o_SetModeMultiPlayer() {
+        o_gameManager.o_gameMode = "Multiplayer";
+        o_gameManager.o_totalPlayerCount = 2;
+
+    }
+
+    public void o_LoadOptionsPanel() {
+        // Here we load options panel and hide main panel
+        Debug.Log("Loading options panel");
+    }
+
+    public void o_ExitGame()
+    {
+        // Here we Exit the game.
+
+    }
+
+
+
+}
