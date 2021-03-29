f9eb8e9edc87850e2a1c2b451a2c8bc6f44b441d
ONKAR KOLVANKAR <ONKARKOLVANKAR@gmail.com>
2021-03-29 15:08:06 +0530

    MAJOR INPUT ERROR SOLVED
    - The input settings in the Input Manager were to be changed
    - 1st Horizontal2 and Vertical2 axis were using the Joystick type Which need to be Key/Mouse type
    - 2nd Their settings of sensitivity,Dead,snap,invert,etc were not same as that of the Vertical1 and Horizontal1 axis

---
+++ Death Race/Assets/InputAxisTesting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputAxisTesting : MonoBehaviour
+{
+    [SerializeField] int playerNo;
+    String HorizontalInputAxis, VerticalInputAxis;
+
+
+    float inputHorizontal, inputVertical;
+    void Start()
+    {
+        HorizontalInputAxis = "Horizontal" + playerNo.ToString();
+        VerticalInputAxis = "Vertical" + playerNo.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputHorizontal = Input.GetAxis(HorizontalInputAxis);
+        inputVertical = Input.GetAxis(VerticalInputAxis);
+        Debug.Log("Player NO = "+playerNo + " | Horizontal = " + inputHorizontal + " Vertical = " + inputVertical);
+    }
+}
